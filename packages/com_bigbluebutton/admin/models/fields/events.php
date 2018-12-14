b212075e3108061b8e3ddd53df89e45eb481f804
Vladimir <workvladster@gmail.com>
2018-12-13 16:00:14 -0800

    update events.php
    checking permission to view meeting rooms in the selectbox when creating an event at frontend.

--- packages/com_bigbluebutton/admin/models/fields/events.php
+++ packages/com_bigbluebutton/admin/models/fields/events.php
@@ -36,23 +36,29 @@ class JFormFieldEvents extends JFormFieldList
 	public function getOptions()
 	{
 		$db = JFactory::getDBO();
-$query = $db->getQuery(true);
-$query->select($db->quoteName(array('a.id','a.title'),array('id','meeting_id_title')));
-$query->from($db->quoteName('#__bigbluebutton_meeting', 'a'));
-$query->where($db->quoteName('a.published') . ' = 1');
-$query->order('a.title ASC');
-$db->setQuery((string)$query);
-$items = $db->loadObjectList();
-$options = array();
-if ($items)
-{
-	$options[] = JHtml::_('select.option', '', 'Select an option');
-	foreach($items as $item)
-	{
-		$options[] = JHtml::_('select.option', $item->id, $item->meeting_id_title);
-	}
-}
+		$query = $db->getQuery(true);
+		$query->select($db->quoteName(array('a.id','a.title'),array('id','meeting_id_title')));
+		$query->from($db->quoteName('#__bigbluebutton_meeting', 'a'));
+		$query->where($db->quoteName('a.published') . ' = 1');
+		$query->order('a.title ASC');
+		$db->setQuery((string)$query);
+		$items = $db->loadObjectList();
+		$options = array();
+		if ($items)
+		{
+		    $options[] = JHtml::_('select.option', '', 'Select an option');
+		    foreach($items as $nr => &$item)
+		    {
+			$access = (JFactory::getUser()->authorise('meeting.access', 'com_bigbluebutton.meeting.' . (int) $item->id) && JFactory::getUser()->authorise('meeting.access', 'com_bigbluebutton'));
+			if (!$access)
+			{
+			    unset($items[$nr]);
+			    continue;
+			}
+			$options[] = JHtml::_('select.option', $item->id, $item->meeting_id_title);
+		    }
+		}
 
-return $options;
+		return $options;
 	}
 }
